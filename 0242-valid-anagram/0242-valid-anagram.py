b1ed40b5d3a6c507010de1ef7592130d7d0ac53c
Ulviya Dadashova <108259441+Ulviya05@users.noreply.github.com>
2024-09-09 17:13:53 +0400

    Time: 58 ms (14.8%), Space: 16.9 MB (47.23%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -7,8 +7,8 @@ class Solution:
             if s[i] not in dict_s:
                 dict_s[s[i]] = 0
             dict_s[s[i]] += 1
-        for i in range(len(t)):
-            if t[i] not in dict_t:  
+
+            if t[i] not in dict_t:
                 dict_t[t[i]] = 0
             dict_t[t[i]] += 1
         return dict_s == dict_t
