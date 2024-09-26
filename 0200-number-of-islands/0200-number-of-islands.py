7ad14646e9462f4e5ca01501a6dccdd50e4737d4
Ulviya Dadashova <108259441+Ulviya05@users.noreply.github.com>
2024-09-26 16:36:28 +0400

    Time: 200 ms (99.85%), Space: 18.8 MB (83.86%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,14 +3,13 @@ class Solution:
         count = 0
         def dfs(i, j):
             grid[i][j] = "0"
-
-            if i + 1 < len(grid) and grid[i + 1][j] == "1":
+            if i + 1 < len(grid) and grid[i+1][j] == "1":
                 dfs(i + 1, j)
-            if j + 1 < len(grid[0]) and grid[i][j + 1] == "1":
+            if j + 1 < len(grid[0]) and grid[i][j+1] == "1":
                 dfs(i, j + 1)
-            if i - 1 >= 0 and grid[i - 1][j] == "1":
+            if i > 0  and grid[i - 1][j] == "1":
                 dfs(i - 1, j)
-            if j - 1 >= 0 and grid[i][j - 1] == "1":
+            if j > 0 and grid[i][j - 1] == "1":
                 dfs(i, j - 1)
         for i in range(len(grid)):
             for j in range(len(grid[0])):
