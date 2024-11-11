0b9dedfcdfffb1c67b6f58b0517c6432162a1ef4
Ulviya Dadashova <108259441+Ulviya05@users.noreply.github.com>
2024-11-11 14:55:12 +0400

    Time: 65 ms (79.96%), Space: 16.9 MB (99.13%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -4,14 +4,14 @@ class Solution:
             return ""
         countT, window = {}, {}
         for c in t:
-            countT[c] = 1 + countT.get(c, 0)
+            countT[c] = 1 + countT.get(c,0)
         have, need = 0, len(countT)
         res, resLen = [-1, -1], float("infinity")
         l = 0
         for r in range(len(s)):
             c = s[r]
             window[c] = 1 + window.get(c, 0)
-            if c in countT and window[c] == countT[c]:
+            if c in countT and countT[c] == window[c]:
                 have += 1
             while have == need:
                 if r - l + 1 < resLen:
@@ -22,6 +22,7 @@ class Solution:
                     have -= 1
                 l += 1
         l, r = res
-        return s[l : r + 1] if resLen != float("infinity") else ""
+        return s[l:r + 1] if resLen != float("infinity") else ""
+
 
         
