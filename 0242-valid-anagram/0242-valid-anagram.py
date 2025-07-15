7463bbd82cae30cefd686217cbf810d95bf84535
Ulviya Dadashova <108259441+ulviyadadashova@users.noreply.github.com>
2025-07-15 10:26:58 +0400

    Time: 11 ms (72.23%), Space: 17.8 MB (63.95%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,15 +2,14 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        dict_s = {}
-        dict_t = {}
-        for char in s:
-            if char not in dict_s:
-                dict_s[char] = 0
-            dict_s[char] += 1
-        for char in t:
-            if char not in dict_t:
-                dict_t[char] = 0
-            dict_t[char] += 1
-        return dict_s == dict_t
+        dict_s, dict_t = {}, {}
+        for i in range(len(s)):
+            dict_s[s[i]] = 1 + dict_s.get(s[i], 0 )
+            dict_t[t[i]] = 1 + dict_t.get(t[i], 0 )
+        return dict_t == dict_s
+# Time - O(n)
+# Space - O(n)
+# Brute - sort the arrays then check if same but time is O(nlogn)
+
+
             
