4247b7a74f581d9fa2de6685c83b48d869b38649
Ulviya Dadashova <108259441+ulviyadadashova@users.noreply.github.com>
2025-07-15 19:59:15 +0400

    Time: 11 ms (24.12%), Space: 24.2 MB (13.73%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -4,14 +4,20 @@ class Solution:
         dic = {}
         result = []
         for num in nums:
-            dic[num] = dic.get(num, 0) + 1
-        for el, freq in  dic.items():
-            bucket[freq].append(el)
+            dic[num] = 1 + dic.get(num, 0)
+        for num, count in dic.items():
+            bucket[count].append(num)
         for i in range(len(bucket) - 1, 0, -1):
-            for num in bucket[i]:
-                result.append(num)
+            for n in bucket[i]:
+                result.append(n)
                 if len(result) == k:
                     return result
+# Time - O(n)
+# Space - O(n)
+# Brute - iterate over array and implement count opration to each number and find the most frequent number this way and add it to result array. if its lenght is not k then continue checking but the most frequest one cant be if it is in result array already so it will be for loop in while loop so the time will be o(n square)
+
+
+
 
 
 
