af05470c06baffda9384990660e3e278a9e8530b
Ulviya Dadashova <108259441+ulviyadadashova@users.noreply.github.com>
2025-07-15 10:54:38 +0400

    Time: 2 ms (53.38%), Space: 19.2 MB (10.43%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,6 +5,9 @@ class Solution:
             if target - n in dic:
                 return [dic[target - n], i]
             dic[n] = i
+# Time - O(n)
+# Space - O(n)
+# Brute - compare each number with every other to check if their sum is equal to target but time will be n square
         
         
         
