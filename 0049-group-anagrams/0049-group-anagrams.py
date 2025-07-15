959b786de2d586a0a815417c9ddfc8e75ddd4c55
Ulviya Dadashova <108259441+ulviyadadashova@users.noreply.github.com>
2025-07-15 13:42:18 +0400

    Time: 13 ms (55.12%), Space: 20.4 MB (95.7%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,13 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = {}
+        group = {}
         for word in strs:
             sorted_word = "".join(sorted(word))
-            if sorted_word in res:
-                res[sorted_word].append(word)
+            if sorted_word in group:
+                group[sorted_word].append(word)
             else:
-                res[sorted_word] = [word]
-        return list(res.values())
-
-
-        
+                group[sorted_word] = [word]
+        return list(group.values())
+# Time - O(n * klogk)
+# Space - O(nk)
+# Brute - get every pair of words sort them and compare if they are anagrams time will be n square * klogk
