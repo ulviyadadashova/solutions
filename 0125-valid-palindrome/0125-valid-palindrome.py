c63fd778f9cc77deb027a1a79e383fdd20134aa4
Ulviya Dadashova <108259441+ulviyadadashova@users.noreply.github.com>
2025-07-16 14:51:51 +0400

    Time: 7 ms (81.08%), Space: 17.9 MB (83.23%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,15 +1,18 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        l, r = 0, len(s) - 1
-        while l < r:
-            if not s[l].isalnum():
-                l += 1
-            elif not s[r].isalnum():
-                r -= 1
-            elif s[l].lower() != s[r].lower():
+        L, R = 0, len(s) - 1
+        while L < R:
+            if not s[L].isalnum():
+                L += 1
+            elif not s[R].isalnum():
+                R -= 1
+            elif s[L].lower() != s[R].lower():
                 return False
             else:
-                l += 1
-                r -= 1
+                L += 1
+                R -= 1
         return True
-        
+
+# Time - O(n)
+# Space - O(1)
+# Brute - iterate over string and create filtered one with only alpha and compare string with its reversed version time is O(n) space is O(n)
