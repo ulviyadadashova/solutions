677f07fc4e2c4afcb41a4e7a557aaf5040eb10a3
Ulviya Dadashova <108259441+ulviyadadashova@users.noreply.github.com>
2025-07-16 17:24:13 +0400

    Time: 528 ms (77.12%), Space: 20.7 MB (36.71%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -3,21 +3,25 @@ class Solution:
         res = []
         nums.sort()
         for i, a in enumerate(nums):
-            if i > 0 and a == nums[i - 1]:
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
             l, r = i + 1, len(nums) - 1
             while l < r:
                 threeSum = nums[l] + nums[r] + a
-                if threeSum < 0:
-                    l += 1
-                elif threeSum > 0:
+                if threeSum > 0:
                     r -= 1
+                elif threeSum < 0:
+                    l += 1
                 else:
                     res.append([a, nums[l], nums[r]])
                     l += 1
+                    r -= 1
                     while l < r and nums[l] == nums[l - 1]:
-                        l += 1
+                        l += 1   
         return res
+# Time - O(n square)
+# Space - O(n + m)
+# Brute - turn into set then using three nested loops and finding sum of every combination time is O(n triple) space is O(m)
                     
 
 
