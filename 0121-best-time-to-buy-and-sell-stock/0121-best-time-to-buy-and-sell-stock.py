7da39d15e3116090045e7e7bbbc10ae46687c34c
Ulviya Dadashova <108259441+ulviyadadashova@users.noreply.github.com>
2025-07-20 12:05:22 +0400

    Time: 84 ms (58.68%), Space: 26.9 MB (32.45%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,13 +1,21 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         window_start = 0
-        max_profit = 0 
+        max_profit = 0
         for window_end in range(len(prices)):
-            if prices[window_start] < prices[window_end]:
-                max_profit = max(max_profit, prices[window_end] - prices[window_start])
-            else:
+            buy_price = prices[window_start]
+            sell_price = prices[window_end]
+            if buy_price >= sell_price:
                 window_start = window_end
+            else:
+                max_profit = max(max_profit, sell_price - buy_price)
         return max_profit
+
+# Time - O(n)
+# Space - O(1)
+# Brute - two nested loops to check each combination and find max_profit but time is O(n square)
+
+
             
 
 
