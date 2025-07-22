bbbc78ba96bcae5e292c02f1559c2d489ad292ba
Ulviya Dadashova <108259441+ulviyadadashova@users.noreply.github.com>
2025-07-22 20:40:05 +0400

    Time: 14 ms (84.43%), Space: 18.1 MB (12.22%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,15 +1,21 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int: 
-        window_start = 0 
+        window_start = 0
         char_frequency = {}
         max_length = 0
         for window_end in range(len(s)):
             right_char = s[window_end]
-            if right_char in  char_frequency and window_start <= char_frequency[right_char]:
+            if right_char in char_frequency and window_start <= char_frequency[right_char]:
                 window_start = char_frequency[right_char] + 1
             char_frequency[right_char] = window_end
             max_length = max(max_length, window_end - window_start + 1)
         return max_length
+# Time - O(n)
+# Space - O(n)
+# Brute - Check all substrings using two nested loops. For each substring, check for duplicates using a set. but time is O(n square)
+                
+
+        
 
             
 
